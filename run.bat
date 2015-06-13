c19c5f13630b9b6fad53fb95177622e92c0d7f9f
Puneet Gupta <pgupta@salesforce.com>
2015-06-12 17:51:13 -0700

    fixed bug in windows script

--- run.bat
+++ run.bat
@@ -2,12 +2,12 @@
 SET BASEDIR=%~dp0.
 cd %BASEDIR%
 
-FOR /F "delims=|" %%I IN ('DIR /B /O:D') DO SET LATEST_JAR=%%I
+FOR /F "delims=|" %%I IN ('DIR datasetutils*.jar /B /O:D') DO SET LATEST_JAR=%%I
 echo %LATEST_JAR%
 
 java -cp .;%LATEST_JAR% com.sforce.dataset.util.UpgradeChecker
 
-FOR /F "delims=|" %%I IN ('DIR /B /O:D') DO SET LATEST_JAR=%%I
+FOR /F "delims=|" %%I IN ('DIR datasetutils*.jar /B /O:D') DO SET LATEST_JAR=%%I
 echo %LATEST_JAR%
 
 java -Xmx1G -jar %LATEST_JAR% --server true
