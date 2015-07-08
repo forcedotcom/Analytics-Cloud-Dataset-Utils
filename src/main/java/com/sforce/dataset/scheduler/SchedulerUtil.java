cc4f22815e9015e64e47909d69e139bb44fe34f7
Puneet Gupta <pgupta@salesforce.com>
2015-07-08 13:55:15 -0700

    Data flow Scheduler and Multiple Dataflows

--- src/main/java/com/sforce/dataset/scheduler/SchedulerUtil.java
+++ src/main/java/com/sforce/dataset/scheduler/SchedulerUtil.java
@@ -32,6 +32,9 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -41,11 +44,14 @@ import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.quartz.CronTrigger;
 import org.quartz.JobDetail;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
+import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -73,7 +79,14 @@ public class SchedulerUtil {
 			 {
 				 try {
 					 Schedule sched = mapper.readValue(file, Schedule.class);
-					list.add(sched);
+					 if(sched.getDevName()!=null)
+					 {
+						 if(!sched.isDisabled())
+						 {
+							 sched.setNextRunTime(getScheduledNextRunTime(partnerConnection, sched));
+						 }
+						 list.add(sched);
+					 }
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -82,35 +95,33 @@ public class SchedulerUtil {
 		return list;
 	}
 	
-	public static void saveSchedule(PartnerConnection partnerConnection, String scheduleName, String jobType, String jobId, String cronSchedule, Map<?,?> jobDataMap) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException
+	public static void saveSchedule(PartnerConnection partnerConnection, String scheduleName, String cronSchedule, String frequency,long interval, Map<?,?> jobDataMap, long scheduleStartDate, boolean isCreate) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException
 	{
-		String orgId = partnerConnection.getUserInfo().getOrganizationId();
-		File dir = DatasetUtilConstants.getScheduleDir(orgId);
 		String masterLabel = scheduleName;
 		String devName = ExternalFileSchema.createDevName(scheduleName, "Schedule", 1, false);
-		File file = new File(dir,devName+".json");
 		Schedule dg = new Schedule();
 		dg.setDevName(devName);
 		dg.setMasterLabel(masterLabel);
+		dg.setFrequency(frequency);
+		dg.setInterval(interval);
+		dg.setScheduleStartDate(scheduleStartDate);
 		dg.setCronSchedule(cronSchedule);
-		dg.setJobType(jobType);
-		dg.setJobId(jobId);
 		dg.setJobDataMap(jobDataMap);
-		ObjectMapper mapper = new ObjectMapper();	
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		mapper.writerWithDefaultPrettyPrinter().writeValue(file, dg);
+		dg.set_LastModifiedBy(partnerConnection);
+		writeSchedule(partnerConnection, dg, isCreate);
 		return;
 	}
 	
 
-	public static void deleteSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException
+	public static void deleteSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException, SchedulerException
 	{
 		String orgId = partnerConnection.getUserInfo().getOrganizationId();
-		File dir = DatasetUtilConstants.getDataflowGroupDir(orgId);
-		String devName = ExternalFileSchema.createDevName(scheduleName, "dataFlowGroup", 1, false);
+		File dir = DatasetUtilConstants.getScheduleDir(orgId);
+		String devName = ExternalFileSchema.createDevName(scheduleName, "schedule", 1, false);
 		File file = new File(dir,devName+".json");
 		if(file.exists())
 		{
+			disableSchedule(partnerConnection, devName);
 			file.delete();
 		}else
 		{
@@ -119,24 +130,41 @@ public class SchedulerUtil {
 	}
 	
 	
-	public static void disableSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException
+	public static void disableSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException, SchedulerException
 	{
-
-
+		Schedule sched = readSchedule(partnerConnection, scheduleName);
+		stopSchedule(partnerConnection, sched);
+    	sched.setDisabled(true);
+    	writeSchedule(partnerConnection, sched, false);
 	}
 	
-	public static void enableSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException
+	public static void enableSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException, SchedulerException
 	{
-
-
+		Schedule sched = readSchedule(partnerConnection, scheduleName);  
+		startSchedule(sched, partnerConnection);
+        sched.setDisabled(false);
+        writeSchedule(partnerConnection, sched, false);
 	}
 	
-
-	public static void startSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException, SchedulerException
+	public static void writeSchedule(PartnerConnection partnerConnection, Schedule schedule, boolean isCreate) throws ConnectionException, JsonParseException, JsonMappingException, IOException
 	{
 		String orgId = partnerConnection.getUserInfo().getOrganizationId();
-		File dir = DatasetUtilConstants.getDataflowGroupDir(orgId);
-		String devName = ExternalFileSchema.createDevName(scheduleName, "dataFlowGroup", 1, false);
+		File dir = DatasetUtilConstants.getScheduleDir(orgId);
+		File file = new File(dir,schedule.getDevName()+".json");
+		if(isCreate && file.exists())
+		{
+			throw new IllegalArgumentException("Schedule {"+schedule.getDevName()+"} already exists in the system");
+		}
+		ObjectMapper mapper = new ObjectMapper();	
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.writerWithDefaultPrettyPrinter().writeValue(file, schedule);
+	}
+
+	public static Schedule readSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonParseException, JsonMappingException, IOException
+	{
+		String orgId = partnerConnection.getUserInfo().getOrganizationId();
+		File dir = DatasetUtilConstants.getScheduleDir(orgId);
+		String devName = ExternalFileSchema.createDevName(scheduleName, "schedule", 1, false);
 		File file = new File(dir,devName+".json");
 		if(!file.exists())
 		{
@@ -145,40 +173,202 @@ public class SchedulerUtil {
 		ObjectMapper mapper = new ObjectMapper();	
 		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		Schedule sched = mapper.readValue(file, Schedule.class);
-		
-        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-               
-        if(sched.getJobType().equalsIgnoreCase("dataflowGroup"))
-        {
-        	if(!scheduler.checkExists(jobKey(sched.getDevName(), "dataflowGroup")))
-        	{
-        		JobDetail job  = newJob(DataflowJob.class)
-    			    .withIdentity(sched.getDevName(), "dataflowGroup")
-    			    .build();
-
-        		if(sched.getJobDataMap()!=null)
-        		{
-	        		for(Object key:sched.getJobDataMap().keySet())
-	        		{
-	        			job.getJobDataMap().put(key.toString(), sched.getJobDataMap().get(key));   
-	        		}
-        		}
-        		
-                CronTrigger trigger = newTrigger()
-                	    .withIdentity(sched.getDevName()+"_trigger", "dataflowGroup")
-                	    .withSchedule(cronSchedule(sched.getCronSchedule()))
-                	    .build();
-                
-                scheduler.scheduleJob(job, trigger);
-        	}
-        }else
-        {
-        	throw new IllegalArgumentException("schedule {"+scheduleName+"} is already started");
-        }
-
-        if(scheduler.isStarted())
-        	scheduler.start();		
+		return sched;
 	}
+
+//	public static void startSchedule(PartnerConnection partnerConnection, String scheduleName) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException, SchedulerException
+//	{
+////		String orgId = partnerConnection.getUserInfo().getOrganizationId();
+////		File dir = DatasetUtilConstants.getScheduleDir(orgId);
+////		String devName = ExternalFileSchema.createDevName(scheduleName, "schedule", 1, false);
+////		File file = new File(dir,devName+".json");
+////		if(!file.exists())
+////		{
+////			throw new IllegalArgumentException("Schedule {"+scheduleName+"} does not exist in the system");
+////		}
+////		ObjectMapper mapper = new ObjectMapper();	
+////		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//
+//		Schedule sched = readSchedule(partnerConnection, scheduleName);
+//		
+//        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+//               
+//        if(sched.getJobType().equalsIgnoreCase("schedule"))
+//        {
+//        	if(!scheduler.checkExists(jobKey(sched.getDevName(), "schedule")))
+//        	{
+//        		JobDetail job  = newJob(DataflowJob.class)
+//    			    .withIdentity(sched.getDevName(), "schedule")
+//    			    .build();
+//
+//        		if(sched.getJobDataMap()!=null)
+//        		{
+//	        		for(Object key:sched.getJobDataMap().keySet())
+//	        		{
+//	        			job.getJobDataMap().put(key.toString(), sched.getJobDataMap().get(key));   
+//	        		}
+//        		}
+//        		
+//                CronTrigger trigger = newTrigger()
+//                	    .withIdentity(sched.getDevName()+"_trigger", "schedule")
+//                	    .withSchedule(cronSchedule(sched.getCronSchedule()))
+//                	    .build();
+//                
+//                scheduler.scheduleJob(job, trigger);
+//        	}
+//        }else
+//        {
+//        	throw new IllegalArgumentException("schedule {"+scheduleName+"} is already started");
+//        }
+//
+//        if(scheduler.isStarted())
+//        	scheduler.start();		
+//	}
+
+	
+	public static String getCronSchedule(Schedule sched)
+	{
+		if(sched!=null)
+		{
+			if(sched.getCronSchedule()!=null && !sched.getCronSchedule().isEmpty())
+				return sched.getCronSchedule();
+			else
+			{
+				Date dt = new Date(sched.getScheduleStartDate());
+				Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+				calendar.setTime(dt);   // assigns calendar to given date 
+				int minute = calendar.get(Calendar.MINUTE);
+				int hour = calendar.get(Calendar.HOUR);
+				int day = calendar.get(Calendar.DAY_OF_MONTH);
+				int dayofweek = calendar.get(Calendar.DAY_OF_WEEK);
+				long interval = sched.getInterval();
+				
+				if(sched.getFrequency().equalsIgnoreCase("day"))
+				{
+					if(interval == 1)
+						return String.format("0 %d %d * * ?",minute,hour);
+					else
+						return String.format("0 %d %d * * ?",minute,hour);
+						
+				} else if(sched.getFrequency().equalsIgnoreCase("hour"))
+				{
+					if(interval == 1)
+						return String.format("0 %d * * * ?",minute);
+					else
+						return String.format("0 %d */%d * * ?",minute,interval);
+				} else if(sched.getFrequency().equalsIgnoreCase("minute"))
+				{
+					if(interval == 1)
+						return "0 * * * * ?";
+					else
+						return String.format("0 */%d * * * ?",interval);
+				} else if(sched.getFrequency().equalsIgnoreCase("week"))
+				{
+					if(interval == 1)
+						return String.format("0 %d %d ? * %d",minute,hour,dayofweek);
+					else
+						return String.format("0 %d %d ? * %d",minute,hour,dayofweek);
+				} else if(sched.getFrequency().equalsIgnoreCase("month"))
+				{
+					if(interval == 1)
+						return String.format("0 %d %d %d * ?",minute,hour,day);
+					else
+						return String.format("0 %d %d %d * ?",minute,hour,day);
+				}
+			}
+		}		
+		return null;
+	}
+	
+	public static void startAllSchedules(PartnerConnection partnerConnection) throws ConnectionException, SchedulerException
+	{
+		List<Schedule> list = listSchedules(partnerConnection);
+		for(Schedule sched:list)
+		{
+			if(!sched.isDisabled())
+			{
+				try
+				{
+					startSchedule(sched, partnerConnection);
+				}catch(Exception t)
+				{
+					t.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static void startSchedule(Schedule sched, PartnerConnection partnerConnection) throws SchedulerException
+	{
+	       Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+	        if(sched.getJobType().equalsIgnoreCase("dataflow"))
+	        {
+	        	JobKey jkey = jobKey(sched.getDevName(), "dataflow");
+
+	        	if(!scheduler.checkExists(jkey))
+	        	{
+	        		JobDetail job  = newJob(DataflowJob.class)
+	    			    .withIdentity(sched.getDevName(), "dataflow")
+	    			    .build();
+
+	        		if(sched.getJobDataMap()!=null)
+	        		{
+		        		for(Object key:sched.getJobDataMap().keySet())
+		        		{
+		        			job.getJobDataMap().put(key.toString(), sched.getJobDataMap().get(key));   
+		        		}
+	        		}
+	        		        		
+	                CronTrigger trigger = newTrigger()
+	                	    .withIdentity(sched.getDevName()+"_trigger", "dataflow")
+	                	    .withSchedule(cronSchedule(getCronSchedule(sched)))
+	                	    .build();
+	                
+	                scheduler.getContext().put("conn", partnerConnection);
+	                
+	                Date temp = scheduler.scheduleJob(job, trigger);
+	                
+	                System.out.println("Schedule job {"+sched.getDevName()+"} scheduled next run {"+temp+"}");
+	                
+	                if(!scheduler.isStarted())
+	                	scheduler.start();	
+	        	}
+	        }
+	}
+	
+	
+	public static void stopSchedule(PartnerConnection partnerConnection, Schedule sched) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException, SchedulerException
+	{
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        if(sched.getJobType().equalsIgnoreCase("dataflow"))
+        {
+        	JobKey jkey = jobKey(sched.getDevName(), "dataflow");
+        	if(scheduler.checkExists(jkey))
+        	{        
+        		scheduler.deleteJob(jkey);
+        	}
+        }
+	}
+	
+	public static long getScheduledNextRunTime(PartnerConnection partnerConnection, Schedule sched) throws ConnectionException, JsonGenerationException, JsonMappingException, IOException, SchedulerException
+	{
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        if(sched.getJobType().equalsIgnoreCase("dataflow"))
+        {
+        	JobKey jkey = jobKey(sched.getDevName(), "dataflow");
+        	if(scheduler.checkExists(jkey))
+        	{        
+                List<? extends Trigger> triggers = scheduler.getTriggersOfJob(jkey);
+                if (triggers.size() > 0)
+                {
+                    Date dt = triggers.get(0).getNextFireTime();
+                    return dt.getTime();
+                }
+            }
+        }
+		return 0;
+	}
+	
 
 
 
